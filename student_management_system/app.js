7efe5e65508d47bd32836417e67f1d4a1aeb88d0
SaniyaKandharkar <78016661+SaniyaKandharkar@users.noreply.github.com>
2021-03-18 23:41:42 +0530

    Add files via upload

---
+++ student_management_system/app.js
@@ -0,0 +1,28 @@
+require('./models/db')
+
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const bodyparser = require('body-parser')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+
+const studentController = require('./controllers/studentController')
+
+const app = express()
+
+app.use(bodyparser.urlencoded({
+    extended:true
+}))
+app.use(bodyparser.json())
+
+
+app.set('views', path.join(__dirname, '/views/'))
+app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/'}))
+app.set('view engine', 'hbs')
+
+app.listen(3000, () => {
+    console.log('Server running')
+})
+
+
+app.use('/student', studentController)
